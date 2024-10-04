5fc14029541d577733a7922ae1f62f3e5f43e770
martasavenok <magnolia.sav@gmail.com>
2024-10-05 01:40:51 +0300

    end

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
+
     document.querySelectorAll('.ripple').forEach(function(button) {
         button.addEventListener('mouseover', function(event) {
             event.preventDefault();
 
-            // Создание элемента ripple
             let div = document.createElement('div');
             let btnOffset = this.getBoundingClientRect();
             let xPos = event.pageX - btnOffset.left - window.scrollX;
@@ -11,20 +11,74 @@ document.addEventListener('DOMContentLoaded', function() {
 
             div.classList.add('ripple-effect');
 
-            div.style.height = this.offsetHeight + 'px';
-            div.style.width = this.offsetHeight + 'px';
-            div.style.top = yPos - (this.offsetHeight / 2) + 'px';
-            div.style.left = xPos - (this.offsetHeight / 2) + 'px';
-            div.style.background = this.getAttribute('data-ripple-color');
+            let ripple = this.querySelector('.ripple-effect');
 
-            this.appendChild(div);
+            if (!ripple) {
+                div.style.height = this.offsetHeight + 'px';
+                div.style.width = this.offsetHeight + 'px';
+                div.style.top = yPos - (this.offsetHeight / 2) + 'px';
+                div.style.left = xPos - (this.offsetHeight / 2) + 'px';
+                div.style.background = this.getAttribute('data-ripple-color');
 
-            button.addEventListener('mouseleave', function() {
-                div.remove();
-            }, { once: true });
+                this.appendChild(div);
+
+                window.setTimeout(function(){
+                    div.remove();
+                }, 1000);
+            }
         });
     });
+
 });
+
+const heroSwiper = document.querySelector('.hero-swiper');
+
+if (heroSwiper) {
+    const swiper = new Swiper('.swiper', {
+        loop: true,
+        pagination: {
+            el: '.hero-swiper .custom-swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.hero-swiper-button-next',
+            prevEl: '.hero-swiper-button-prev',
+        },
+    });
+}
+
+// custom select
+
+const servicesSelect =  document.querySelector('.services');
+const servicesOptions = document.querySelectorAll('.services-list li');
+const servicesSelectBody = document.querySelector('.services-list');
+
+
+if (servicesSelect) {
+    servicesSelect.addEventListener('click' , () => {
+        servicesSelectBody.classList.add('active');
+    })
+    servicesSelect.querySelector('p').addEventListener('click' , () => {
+        servicesSelectBody.classList.add('active');
+    })
+    servicesOptions.forEach(el => {
+        el.addEventListener('click' , ()=> {
+            servicesSelect.querySelector('p').innerHTML = el.innerHTML;
+            servicesSelect.dataset.current = el.dataset.value;
+            servicesSelectBody.classList.remove('active')
+        })
+    })
+    document.addEventListener('click' , (e)=> {
+        if ((!e.target.closest('.services-wrap')) && servicesSelectBody.classList.contains('active')) {
+            servicesSelectBody.classList.remove('active');
+        }
+    })
+}
+
+
+
+
+
 
 
 
